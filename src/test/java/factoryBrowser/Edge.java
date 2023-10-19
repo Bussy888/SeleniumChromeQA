791d621d1fc4feb6288c5c13a21293b452a52f83
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2023-10-18 21:34:34 -0400

    edge implementation

--- src/test/java/factoryBrowser/Edge.java
+++ src/test/java/factoryBrowser/Edge.java
@@ -1,10 +1,14 @@
 package factoryBrowser;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class Edge implements IBrowser{
+    EdgeDriver edgeDriver;
     @Override
     public WebDriver create() {
-        return null;
+        System.setProperty("webdriver.edge.driver","src/test/resources/edge/msedgedriver.exe");
+        edgeDriver = new EdgeDriver();
+        return edgeDriver;
     }
 }
