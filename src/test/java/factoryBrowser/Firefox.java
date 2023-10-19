d100f26e4d3fb561a411e05806af5eba70f90145
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2023-10-18 21:22:26 -0400

    Firefox implementation

--- src/test/java/factoryBrowser/Firefox.java
+++ src/test/java/factoryBrowser/Firefox.java
@@ -1,11 +1,15 @@
 package factoryBrowser;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Firefox implements IBrowser{
+    FirefoxDriver firefoxDriver;
 
     @Override
     public WebDriver create() {
-        return null;
+        System.setProperty("webdriver.gecko.driver","src/test/resources/firefox/geckodriver.exe");
+        firefoxDriver = new FirefoxDriver();
+        return firefoxDriver;
     }
 }
